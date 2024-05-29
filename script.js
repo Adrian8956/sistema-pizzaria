ee20cd3b91b3c1ddfae6ec8709141fab0acec236
Adrian8956 <adrianalmeidaczs271@gmail.com>
2024-05-29 11:29:24 -0300

    finalizando o projeto

--- script.js
+++ script.js
@@ -19,10 +19,29 @@ frm.inDetalhes.addEventListener("focus", () => {
 
     const num = pizza == "media" ? 2 : pizza == "grande" ? 3 : 4;
 
-    frm.inDetalhes.placeholder = `Até ${num} sabores`
+    frm.inDetalhes.placeholder = `Até ${num} sabores`;
    }
 });
 
 frm.inDetalhes.addEventListener("blur", () =>{
-    frm.inDetalhes.placeholder = ""
+    frm.inDetalhes.placeholder = "";
+})
+
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    let produto;
+    if(frm.rbPizza.checked){
+        const num = frm.inPizza.selectedIndex; // obtem o n° do item selecionado
+        produto = frm.inBebida.options[num].text; // texto do item selecionado
+    }else{
+        const num = frm.inBebida.selectedIndex;
+        produto = frm.inBebida.options[num].text;
+    }
+
+    const detalhes = frm.inDetalhes.value;
+    itens.push(produto + "(" + detalhes + ")");
+    resp.innerText = itens.join("\n");
+
+    frm.reset();
+    frm.rbPizza.dispatchEvent(new Event("click"));
 })
